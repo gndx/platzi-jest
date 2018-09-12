3b5c79a7171de36d1c96b65082199cb4436ff347
Oscar Barajas <oscar@dragons.mx>
2018-09-12 11:26:54 -0500

    snapshots_exceptions

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,23 @@ describe('Es hora de las intantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Siempre fallara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepsion dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Oscar Barajas Tavares"
+        }
+        expect(user).toMatchSnapshot({
+            id:  expect.any(Number)
+        });
+    });
+
 });
